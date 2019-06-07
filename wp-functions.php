028c9e90989496d1be4f2d5993d1961f7c018faa
Troy Chaplin <troy.chaplin@gmail.com>
2019-06-06 22:23:16 -0400

    Adding base index and plugin files

---
+++ wp-functions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Plugin Name: WP Functions
+	Plugin URI: http://troychaplin.ca
+	Description: Adds admin, global and public specific functions
+	Version: 1.0
+	Author: Troy Chaplin
+	Author URI: http://troychaplin.ca
+	License: GPL2
+*/
