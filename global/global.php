e36880b6774a897f79342e6cbb90fe103facbfe1
Troy Chaplin <troy.chaplin@gmail.com>
2019-07-11 08:33:28 -0400

    Removed function that should be tied to ACF functionality

--- global/global.php
+++ global/global.php
@@ -1,16 +1,2 @@
 <?php
-/**
- * Disable gutenberg front end styles
- * =============
- *
- * @package Global Supports
- * @category Gutenberg
- * @version 1.0
- */
-
-// TODO: wrap in acf option
-
-function wps_deregister_styles() {
-	wp_dequeue_style( 'wp-block-library' );
-}
-add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
+// Silence is golden
